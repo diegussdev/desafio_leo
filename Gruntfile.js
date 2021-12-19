e246180cd2177d208612482e1d24897d53b1e8e9
Diego P. Costa <diegussdev@gmail.com>
2021-12-19 13:49:50 -0300

    Estrutura GruntJs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,29 @@
 module.exports = grunt => {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'public/assets/css/main.min.css': ['public/assets/.grunt/sass/main.scss']
+                }
+            }
+        },
+        uglify: {
+            options: {
+                mangle: true
+            },
+            file_min_js: {
+                files: {
+                    'public/assets/js/main.min.js': 'public/assets/.grunt/js/main.js'
+                }
+            }
+        },
+    });
 
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('minify', ['sass', 'uglify']);
 };
